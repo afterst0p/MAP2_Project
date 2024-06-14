acdf75730dd85e51c73443bbc5b7ada726139718
afterst0p <kingwlgn@gmail.com>
2024-06-14 10:53:54 +0900

    Feat: "결제일순 정렬, 월간/연간 구독 보기 옵션 추가"

--- Subscription Fee Management/Subscription Fee Management/Source/User Interface/Subscription/SubscriptionPage.swift
+++ Subscription Fee Management/Subscription Fee Management/Source/User Interface/Subscription/SubscriptionPage.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct SubscriptionPage: View {
     @ObservedObject var subscriptionList: SubscriptionList
@@ -18,7 +19,12 @@ struct SubscriptionPage: View {
     @State private var selectedPaymentPay: Payment.method?
     @State private var selectedPaymentId = ""
     @State private var isUsingShowingOption = false
+    @State private var isShowingMonthly = true
+    @State private var isShowingYearly = true
     @State private var isAddingSubscription = false
+    
+    private let today = Date()
+    private let calendar = Calendar.current
     
     var body: some View {
         NavigationStack(path: $stackPath) {
@@ -114,11 +120,87 @@ struct ListCell: View {
 
 extension SubscriptionPage {
     enum sortMethod: String, CaseIterable {
-        case nameUp = "가나다순", nameDown = "가나다 역순", priceUp = "낮은 가격순", priceDown = "높은 가격순"
+        case payDateUp = "결제일 가까운순", payDateDown = "결제일 먼순", nameUp = "가나다순", nameDown = "가나다 역순", priceUp = "낮은 가격순", priceDown = "높은 가격순"
     }
     
     func sortSelect() -> (Subscription, Subscription) -> Bool {
         switch (selectedSortMethod) {
+        case .payDateUp, .none:
+            return {
+                let todayDateComponent = calendar.dateComponents([.year, .month, .day], from: today)
+                
+                // 월간 구독일 경우 금월로 여김, 날짜 지났으면 익월로 여김
+                // 첫번째 구독 날짜 계산
+                var firstDateComponent = $0.payDate
+                firstDateComponent.year = todayDateComponent.year
+                if !$0.yearly {
+                    firstDateComponent.month = todayDateComponent.month
+                    if (firstDateComponent.day! < todayDateComponent.day!) {
+                        firstDateComponent.month! += 1
+                    }
+                }
+                let firstDate = calendar.date(from: firstDateComponent)!
+                
+                // 두 번째 구독 날짜 계산
+                var secondDateComponent = $1.payDate
+                secondDateComponent.year = todayDateComponent.year
+                if !$1.yearly {
+                    secondDateComponent.month = todayDateComponent.month
+                    if (secondDateComponent.day! < todayDateComponent.day!) {
+                        secondDateComponent.month! += 1
+                    }
+                }
+                let secondDate = calendar.date(from: secondDateComponent)!
+                
+                // 오늘과 차이 계산
+                var firstDistance = calendar.dateComponents([.day], from: today, to: firstDate).day!
+                var secondDistance = calendar.dateComponents([.day], from: today, to: secondDate).day!
+                
+                // 0 미만이면 내년으로 가정 (연간 구독)
+                firstDistance = firstDistance < 0 ? firstDistance + 365 : firstDistance
+                secondDistance = secondDistance < 0 ? secondDistance + 365 : secondDistance
+                
+                // 거리순으로 정렬
+                return firstDistance < secondDistance
+            }
+        case .payDateDown:
+            return {
+                let todayDateComponent = calendar.dateComponents([.year, .month, .day], from: today)
+                
+                // 월간 구독일 경우 금월로 여김, 날짜 지났으면 익월로 여김
+                // 첫번째 구독 날짜 계산
+                var firstDateComponent = $0.payDate
+                firstDateComponent.year = todayDateComponent.year
+                if !$0.yearly {
+                    firstDateComponent.month = todayDateComponent.month
+                    if (firstDateComponent.day! < todayDateComponent.day!) {
+                        firstDateComponent.month! += 1
+                    }
+                }
+                let firstDate = calendar.date(from: firstDateComponent)!
+                
+                // 두 번째 구독 날짜 계산
+                var secondDateComponent = $1.payDate
+                secondDateComponent.year = todayDateComponent.year
+                if !$1.yearly {
+                    secondDateComponent.month = todayDateComponent.month
+                    if (secondDateComponent.day! < todayDateComponent.day!) {
+                        secondDateComponent.month! += 1
+                    }
+                }
+                let secondDate = calendar.date(from: secondDateComponent)!
+                
+                // 오늘과 차이 계산
+                var firstDistance = calendar.dateComponents([.day], from: today, to: firstDate).day!
+                var secondDistance = calendar.dateComponents([.day], from: today, to: secondDate).day!
+                
+                // 0 미만이면 내년으로 가정 (연간 구독)
+                firstDistance = firstDistance < 0 ? firstDistance + 365 : firstDistance
+                secondDistance = secondDistance < 0 ? secondDistance + 365 : secondDistance
+                
+                // 거리순으로 정렬
+                return firstDistance > secondDistance
+            }
         case .nameUp:
             return { $0.name < $1.name }
         case .nameDown:
@@ -127,16 +209,20 @@ extension SubscriptionPage {
             return { $0.price < $1.price }
         case.priceDown:
             return { $0.price > $1.price }
-        case .none:
-            return { $0.name < $1.name }
         }
-        
     }
     
     func filterSelect(subscription: Subscription) -> Bool {
         // 보기 조건 설정 꺼져있을 때 통과
         if (!isUsingShowingOption) {
             return true
+        } else {
+            if (!isShowingMonthly && subscription.yearly == false) {
+                return false
+            }
+            if (!isShowingYearly && subscription.yearly == true) {
+                return false
+            }
         }
         // 카테고리 불일치 시 제외
         if (!selectedCategoryId.isEmpty && selectedCategoryId != subscription.categoryID) {
@@ -166,30 +252,38 @@ extension SubscriptionPage {
                 Text("보기 조건 설정")
             }
             if (isUsingShowingOption) {
-                Picker("카테고리", selection: $selectedCategoryId) {
-                    Text("( 미지정 )").tag("")
-                    ForEach(categoryList.customCategories, id: \.id) { category in
-                        Text(category.name).tag(category.id.uuidString)
+                Section {
+                    Toggle(isOn: $isShowingMonthly) {
+                        Text("월간 구독 보기")
                     }
-                }
-                .pickerStyle(.menu)
-                Picker("결제 수단 유형", selection: $selectedPaymentPay) {
-                    Text("( 미지정 )").tag(nil as Payment.method?)
-                    ForEach(Payment.method.allCases, id: \.self) { method in
-                        Text(method.rawValue).tag(method as Payment.method?)
+                    Toggle(isOn: $isShowingYearly) {
+                        Text("연간 구독 보기")
                     }
-                }
-                .pickerStyle(.menu)
-                if (selectedPaymentPay != nil) {
-                    Picker("결제 수단 이름", selection: $selectedPaymentId) {
+                    Picker("카테고리", selection: $selectedCategoryId) {
                         Text("( 미지정 )").tag("")
-                        ForEach(paymentList.payments, id: \.id) { payment in
-                            if (selectedPaymentPay == payment.pay) {
-                                Text(payment.name).tag(payment.id.uuidString)
-                            }
+                        ForEach(categoryList.customCategories, id: \.id) { category in
+                            Text(category.name).tag(category.id.uuidString)
                         }
                     }
                     .pickerStyle(.menu)
+                    Picker("결제 수단 유형", selection: $selectedPaymentPay) {
+                        Text("( 미지정 )").tag(nil as Payment.method?)
+                        ForEach(Payment.method.allCases, id: \.self) { method in
+                            Text(method.rawValue).tag(method as Payment.method?)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    if (selectedPaymentPay != nil) {
+                        Picker("결제 수단 이름", selection: $selectedPaymentId) {
+                            Text("( 미지정 )").tag("")
+                            ForEach(paymentList.payments, id: \.id) { payment in
+                                if (selectedPaymentPay == payment.pay) {
+                                    Text(payment.name).tag(payment.id.uuidString)
+                                }
+                            }
+                        }
+                        .pickerStyle(.menu)
+                    }
                 }
             }
         }
